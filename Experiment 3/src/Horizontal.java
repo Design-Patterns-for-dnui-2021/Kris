26060366a44fd2559b7ffe96558e91924e56c412
Kristuaji Wiryawan <56579735+wianwiryawan@users.noreply.github.com>
2021-09-14 10:46:17 +0800

    Add files via upload

---
+++ Experiment 3/src/Horizontal.java
@@ -0,0 +1,12 @@
+public class Horizontal extends ScrollBar {
+ 
+	public void scrollH(Scroll scroll) {
+		System.out.println("Horizontal Scroll");
+	}
+	@Override
+	public void displayInfo() {
+		System.out.println("Doing horizontal Scroll");
+	}
+	 
+}
+ 
