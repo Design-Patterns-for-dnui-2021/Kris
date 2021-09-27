2f22679c80cb496c41f5bd651479b4e1a48a4db2
Kristuaji Wiryawan <56579735+wianwiryawan@users.noreply.github.com>
2021-09-27 20:29:11 +0800

    Add files via upload

---
+++ Homework 4 - Pet Simulation Game/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog implements Pet {
+ 
+	@Override
+	public void displayInfo() {
+		System.out.println("A Dog has been created");
+	}
+	 
+}
+ 
---
+++ Homework 4 - Pet Simulation Game/src/Duck.java
@@ -0,0 +1,9 @@
+public class Duck implements Pet {
+ 	
+	@Override
+	public void displayInfo() {
+		System.out.println("A Duck has been created");
+	}
+	 
+}
+ 
